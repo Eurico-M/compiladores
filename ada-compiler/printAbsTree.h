28048fb07495fa4ee07da7faafa26cb919c0fb91
Jin <190189908+asj1n@users.noreply.github.com>
2025-11-09 17:26:54 +0000

    Added methods signatures for consistency

--- ada-compiler/printAbsTree.h
+++ ada-compiler/printAbsTree.h
@@ -1,6 +1,11 @@
 #ifndef PRINT_ABS_TREE_H
 #define PRINT_ABS_TREE_H
 
+void printArExpr(ArExpr* arExpr);
+void printBoolExpr(BoolExpr* boolExpr);
+void printExpr(Expr* expr);
+void printDclr(Dclr* dclr);
+void printPckg(Pckg* pckg);
 void printStm(Stm* stm);
 
 #endif
