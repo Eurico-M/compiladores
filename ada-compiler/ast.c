feae0825a49793c03c3b915c192ef9dbf05ef9b6
Jin <190189908+asj1n@users.noreply.github.com>
2025-12-04 18:53:45 +0000

    Moved typedefs to the top for forward-declaration purposes to prevent errors and cleaned code

--- ada-compiler/ast.c
+++ ada-compiler/ast.c
@@ -3,6 +3,164 @@
 #include "ast.h"
 #include "parser.h"
 
+
+// Packages
+
+Pckg* pckg_simple(char* id) {
+    Pckg* node = (Pckg*) malloc(sizeof(Pckg));
+    node->kind = PCKG_SIMPLE;
+    node->attr.pckg_id = id;
+    return node;
+}
+
+Pckg* pckg_compound(Pckg* first, Pckg* second) {
+    Pckg* node = (Pckg*) malloc(sizeof(Pckg));
+    node->kind = PCKG_COMPOUND;
+    node->attr.compound.first = first;
+    node->attr.compound.second = second;
+    return node;
+}
+
+
+// Arithmetic Expressions
+
+ArExpr* ar_expr_identifier(char* id) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = ID;
+    node->attr.id = id;
+    return node;
+}
+
+ArExpr* ar_expr_integer(int int_val) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = INT;
+    node->attr.int_val = int_val;
+    return node;
+}
+
+ArExpr* ar_expr_float(float float_val) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = FLOAT;
+    node->attr.float_val = float_val;
+    return node;
+}
+
+ArExpr* ar_expr_delimited(ArExpr* delimited_ar_expr) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = DELIMITED_AR_EXPR;
+    node->attr.delimited_ar_expr = delimited_ar_expr;
+    return node;
+}
+
+ArExpr* ar_expr_operation(ar_op op, ArExpr* left, ArExpr* right) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = AR_OP;
+    node->attr.ar_op.op = op;
+    node->attr.ar_op.left = left;
+    node->attr.ar_op.right = right;
+    return node;
+}
+
+ArExpr* ar_expr_boolean(bool bool_ent) {
+    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
+    node->kind = BOOLEAN;
+    node->attr.boolean_ent = bool_ent;
+    return node;
+}
+
+
+// Boolean Expressions
+
+BoolExpr* bool_expr_delimited (BoolExpr* delimited_bool_expr) {
+    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
+    node->kind = DELIMITED_BOOL_EXPR;
+    node->attr.delimited_bool_expr = delimited_bool_expr;
+    return node;
+}
+
+BoolExpr* bool_expr_operation(bool_op op, ArExpr* left, ArExpr* right) {
+    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
+    node->kind = BOOL_OP;
+    node->attr.bool_op.op = op;
+    node->attr.bool_op.left = left;
+    node->attr.bool_op.right = right;
+    return node;
+}
+
+BoolExpr* bool_expr_operation2(bool_op op, BoolExpr* left, BoolExpr* right) {
+    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
+    node->kind = BOOL_OP2;
+    node->attr.bool_op2.op = op;
+    node->attr.bool_op2.left = left;
+    node->attr.bool_op2.right = right;
+    return node;
+}
+
+
+// Expressions
+
+Expr* expr_arithmetic(ArExpr* ar_expr) {
+    Expr* node = (Expr*) malloc(sizeof(Expr));
+    node->kind = EXPR_ARITHMETIC;
+    node->attr.ar_expr = ar_expr;
+    return node;
+}
+
+Expr* expr_boolean(BoolExpr* bool_expr) {
+    Expr* node = (Expr*) malloc(sizeof(Expr));
+    node->kind = EXPR_BOOLEAN;
+    node->attr.bool_expr = bool_expr;
+    return node;
+}
+
+Expr* expr_string(char* string_expr) {
+    Expr* node = (Expr*) malloc(sizeof(Expr));
+    node->kind = EXPR_STRING;
+    node->attr.string_expr = string_expr;
+    return node;
+}
+
+
+// Declarations
+
+Dclr* dclr_compound(Dclr* first, Dclr* second) {
+    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
+    node->kind = DCLR_COMPOUND;
+    node->attr.dclr_compound.first = first;
+    node->attr.dclr_compound.second = second;
+    return node;
+}
+
+// Função auxiliar para converter "Integer" ou "Real" em ST_INTEGER ou ST_REAL
+st_type string_to_st_type(char* type) {
+    if (strcmp(type, "Integer") == 0) {
+        return ST_INTEGER;
+    }
+    else if (strcmp(type, "Real") == 0) {
+        return ST_REAL;
+    }
+
+    yyerror("unkown type");
+}
+
+Dclr* dclr_simple(char* id, char* type) {
+    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
+    node->kind = DCLR_SIMPLE;
+    node->attr.dclr_simple.id = id;
+    node->attr.dclr_simple.type = string_to_st_type(type);
+    return node;
+}
+
+Dclr* dclr_assignment(char* id, char* type, Expr* expr) {
+    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
+    node->kind = DCLR_ASSIGNMENT;
+    node->attr.dclr_assignment.id = id;
+    node->attr.dclr_assignment.type = string_to_st_type(type);
+    node->attr.dclr_assignment.expr = expr;
+    return node;
+}
+
+
 // Statements
 
 Stm* stm_compound(Stm* first, Stm* second) {
@@ -74,157 +232,5 @@ Stm* stm_function(char* func_id, Expr* arg) {
     node->kind = STM_FUNCTION;
     node->attr.stm_func.func_id = func_id;
     node->attr.stm_func.arg = arg;
-    return node;
-}
-
-// Expressions
-
-Expr* expr_arithmetic(ArExpr* ar_expr) {
-    Expr* node = (Expr*) malloc(sizeof(Expr));
-    node->kind = EXPR_ARITHMETIC;
-    node->attr.ar_expr = ar_expr;
-    return node;
-}
-
-Expr* expr_boolean(BoolExpr* bool_expr) {
-    Expr* node = (Expr*) malloc(sizeof(Expr));
-    node->kind = EXPR_BOOLEAN;
-    node->attr.bool_expr = bool_expr;
-    return node;
-}
-
-Expr* expr_string(char* string_expr) {
-    Expr* node = (Expr*) malloc(sizeof(Expr));
-    node->kind = EXPR_STRING;
-    node->attr.string_expr = string_expr;
-    return node;
-}
-
-// Arithmetic Expressions
-
-ArExpr* ar_expr_identifier(char* id) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = ID;
-    node->attr.id = id;
-    return node;
-}
-
-ArExpr* ar_expr_integer(int int_val) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = INT;
-    node->attr.int_val = int_val;
-    return node;
-}
-
-ArExpr* ar_expr_float(float float_val) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = FLOAT;
-    node->attr.float_val = float_val;
-    return node;
-}
-
-ArExpr* ar_expr_delimited(ArExpr* delimited_ar_expr) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = DELIMITED_AR_EXPR;
-    node->attr.delimited_ar_expr = delimited_ar_expr;
-    return node;
-}
-
-ArExpr* ar_expr_operation(ar_op op, ArExpr* left, ArExpr* right) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = AR_OP;
-    node->attr.ar_op.op = op;
-    node->attr.ar_op.left = left;
-    node->attr.ar_op.right = right;
-    return node;
-}
-
-ArExpr* ar_expr_boolean(bool bool_ent) {
-    ArExpr* node = (ArExpr*) malloc(sizeof(ArExpr));
-    node->kind = BOOLEAN;
-    node->attr.boolean_ent = bool_ent;
-    return node;
-}
-
-// Boolean Expressions
-
-BoolExpr* bool_expr_delimited (BoolExpr* delimited_bool_expr) {
-    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
-    node->kind = DELIMITED_BOOL_EXPR;
-    node->attr.delimited_bool_expr = delimited_bool_expr;
-    return node;
-}
-
-BoolExpr* bool_expr_operation(bool_op op, ArExpr* left, ArExpr* right) {
-    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
-    node->kind = BOOL_OP;
-    node->attr.bool_op.op = op;
-    node->attr.bool_op.left = left;
-    node->attr.bool_op.right = right;
-    return node;
-}
-
-BoolExpr* bool_expr_operation2(bool_op op, BoolExpr* left, BoolExpr* right) {
-    BoolExpr* node = (BoolExpr*) malloc(sizeof(BoolExpr));
-    node->kind = BOOL_OP2;
-    node->attr.bool_op2.op = op;
-    node->attr.bool_op2.left = left;
-    node->attr.bool_op2.right = right;
-    return node;
-}
-
-// Declarations
-
-Dclr* dclr_compound(Dclr* first, Dclr* second) {
-    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
-    node->kind = DCLR_COMPOUND;
-    node->attr.dclr_compound.first = first;
-    node->attr.dclr_compound.second = second;
-    return node;
-}
-
-// Função auxiliar para converter "Integer" ou "Real" em ST_INTEGER ou ST_REAL
-st_type string_to_st_type(char* type) {
-    if (strcmp(type, "Integer") == 0) {
-        return ST_INTEGER;
-    }
-    else if (strcmp(type, "Real") == 0) {
-        return ST_REAL;
-    }
-
-    yyerror("unkown type");
-}
-
-Dclr* dclr_simple(char* id, char* type) {
-    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
-    node->kind = DCLR_SIMPLE;
-    node->attr.dclr_simple.id = id;
-    node->attr.dclr_simple.type = string_to_st_type(type);
-    return node;
-}
-
-Dclr* dclr_assignment(char* id, char* type, Expr* expr) {
-    Dclr* node = (Dclr*) malloc(sizeof(Dclr));
-    node->kind = DCLR_ASSIGNMENT;
-    node->attr.dclr_assignment.id = id;
-    node->attr.dclr_assignment.type = string_to_st_type(type);
-    node->attr.dclr_assignment.expr = expr;
-    return node;
-}
-
-// Packages
-
-Pckg* pckg_simple(char* id) {
-    Pckg* node = (Pckg*) malloc(sizeof(Pckg));
-    node->kind = PCKG_SIMPLE;
-    node->attr.pckg_id = id;
-    return node;
-}
-
-Pckg* pckg_compound(Pckg* first, Pckg* second) {
-    Pckg* node = (Pckg*) malloc(sizeof(Pckg));
-    node->kind = PCKG_COMPOUND;
-    node->attr.compound.first = first;
-    node->attr.compound.second = second;
     return node;
 }
--- ada-compiler/ast.h
+++ ada-compiler/ast.h
@@ -1,6 +1,30 @@
 #ifndef __ast_h__
 #define __ast_h__
 
+
+typedef struct _Pckg Pckg;
+typedef struct _ArExpr ArExpr;
+typedef struct _BoolExpr BoolExpr;
+typedef struct _Expr Expr;
+typedef struct _Dclr Dclr;
+typedef struct _Stm Stm;
+
+
+struct _Pckg {
+    enum {
+        PCKG_SIMPLE,
+        PCKG_COMPOUND
+    } kind;
+    union {
+        char* pckg_id;
+        struct {
+            Pckg* first;
+            Pckg* second;
+        } compound;
+    } attr;
+};
+
+
 // Expressões Aritméticas
 
 typedef enum {false, true} bool;
@@ -19,11 +43,11 @@ struct _ArExpr {
         int int_val;
         float float_val;
         bool boolean_ent;
-        struct _ArExpr* delimited_ar_expr;
+        ArExpr* delimited_ar_expr;
         struct {
             ar_op op;
-            struct _ArExpr* left;
-            struct _ArExpr* right;
+            ArExpr* left;
+            ArExpr* right;
         } ar_op;
     } attr;
 };
@@ -40,19 +64,63 @@ struct _BoolExpr {
     } kind;
     union {
         // char* id;
-        struct _BoolExpr* delimited_bool_expr;
+        BoolExpr* delimited_bool_expr;
         struct {
             bool_op op;
-            struct _ArExpr* left;
-            struct _ArExpr* right;
+            ArExpr* left;
+            ArExpr* right;
         } bool_op;
         struct {
             bool_op op;
-            struct _BoolExpr* left;
-            struct _BoolExpr* right;
+            BoolExpr* left;
+            BoolExpr* right;
         } bool_op2;
     } attr;
 };
+
+
+// Expressões
+
+struct _Expr {
+    enum {
+        EXPR_ARITHMETIC,
+        EXPR_BOOLEAN,
+        EXPR_STRING
+    } kind;
+    union {
+        ArExpr* ar_expr;
+        BoolExpr* bool_expr;
+        char* string_expr;
+    } attr;
+};
+
+
+// Declarações
+
+typedef enum {ST_INTEGER, ST_REAL} st_type;
+struct _Dclr {
+    enum {
+        DCLR_COMPOUND,
+        DCLR_SIMPLE,
+        DCLR_ASSIGNMENT
+    } kind;
+    union {
+        struct {
+            Dclr* first;
+            Dclr* second;
+        } dclr_compound;
+        struct {
+            char* id;
+            st_type type;
+        } dclr_simple;          // x: Integer;
+        struct {
+            char* id;
+            st_type type;
+            Expr* expr;
+        } dclr_assignment;      // x: Integer := 1+2;
+    } attr;
+};
+
 
 // Statements
 
@@ -70,117 +138,44 @@ struct _Stm {
     } kind;
     union {
         struct {
-            struct _Stm* first;
-            struct _Stm* second;
+            Stm* first;
+            Stm* second;
         } compound;
         struct {
             char* ident;
-            struct _Expr* expr;
+            Expr* expr;
         } assign;
         struct {
-            struct _Expr* condition;
-            struct _Stm* then_branch;
+            Expr* condition;
+            Stm* then_branch;
         } if_then;
         struct {
-            struct _Expr* condition;
-            struct _Stm* then_branch;
-            struct _Stm* else_branch;
+            Expr* condition;
+            Stm* then_branch;
+            Stm* else_branch;
         } if_then_else;
         struct {
-            struct _Expr* condition;
-            struct _Stm* body;
+            Expr* condition;
+            Stm* body;
         } while_stm;
-        struct _Pckg* pckg_with;
-        struct _Pckg* pckg_use;
+        Pckg* pckg_with;
+        Pckg* pckg_use;
         struct {
             char* proc_id;
-            struct _Dclr* dclr;
-            struct _Stm* body;
+            Dclr* dclr;
+            Stm* body;
         } stm_proc;
         struct {
             char* func_id;
-            struct _Expr* arg;
+            Expr* arg;
         } stm_func;
     } attr;
 };
 
-struct _Pckg {
-    enum {
-        PCKG_SIMPLE,
-        PCKG_COMPOUND
-    } kind;
-    union {
-        char* pckg_id;
-        struct {
-            struct _Pckg* first;
-            struct _Pckg* second;
-        } compound;
-    } attr;
-};
-
-// Declarações
-
-typedef enum {ST_INTEGER, ST_REAL} st_type;
-struct _Dclr {
-    enum {
-        DCLR_COMPOUND,
-        DCLR_SIMPLE,
-        DCLR_ASSIGNMENT
-    } kind;
-    union {
-        struct {
-            struct _Dclr* first;
-            struct _Dclr* second;
-        } dclr_compound;
-        struct {
-            char* id;
-            st_type type;
-        } dclr_simple;          // x: Integer;
-        struct {
-            char* id;
-            st_type type;
-            struct _Expr* expr;
-        } dclr_assignment;      // x: Integer := 1+2;
-    } attr;
-};
-
-// Expressões
-
-struct _Expr {
-    enum {
-        EXPR_ARITHMETIC,
-        EXPR_BOOLEAN,
-        EXPR_STRING
-    } kind;
-    union {
-        struct _ArExpr* ar_expr;
-        struct _BoolExpr* bool_expr;
-        char* string_expr;
-    } attr;
-};
 
 
-typedef struct _Stm Stm;
-typedef struct _Expr Expr;
-typedef struct _ArExpr ArExpr;
-typedef struct _BoolExpr BoolExpr;
-typedef struct _Dclr Dclr;
-typedef struct _Pckg Pckg;
-
-
-Stm* stm_compound(Stm* first, Stm* second);
-Stm* stm_assign(char* ident, Expr* expr);
-Stm* stm_if_then(Expr* condition, Stm* then_branch);
-Stm* stm_if_then_else(Expr* condition, Stm* then_branch, Stm* else_branch);
-Stm* stm_while(Expr* condition, Stm* body);
-Stm* stm_with(Pckg* pckg_with);
-Stm* stm_use(Pckg* pckg_use);
-Stm* stm_procedure(char* proc_id, Dclr* dclr, Stm* body);
-Stm* stm_function(char* func_id, Expr* arg);
-
-Expr* expr_arithmetic(ArExpr* ar_expr);
-Expr* expr_boolean(BoolExpr* bool_expr);
-Expr* expr_string(char* string_expr);
+Pckg* pckg_simple(char* id);
+Pckg* pckg_compound(Pckg* first, Pckg* second);
 
 ArExpr* ar_expr_identifier(char* id);
 ArExpr* ar_expr_integer(int int_val);
@@ -193,11 +188,22 @@ BoolExpr* bool_expr_delimited (BoolExpr* delimited_bool_expr);
 BoolExpr* bool_expr_operation(bool_op op, ArExpr* left, ArExpr* right);
 BoolExpr* bool_expr_operation2(bool_op op, BoolExpr* left, BoolExpr* right);
 
+Expr* expr_arithmetic(ArExpr* ar_expr);
+Expr* expr_boolean(BoolExpr* bool_expr);
+Expr* expr_string(char* string_expr);
+
 Dclr* dclr_compound(Dclr* first, Dclr* second);
 Dclr* dclr_simple(char* id, char* type);
 Dclr* dclr_assignment(char* id, char* type, Expr* expr);
 
-Pckg* pckg_simple(char* id);
-Pckg* pckg_compound(Pckg* first, Pckg* second);
+Stm* stm_compound(Stm* first, Stm* second);
+Stm* stm_assign(char* ident, Expr* expr);
+Stm* stm_if_then(Expr* condition, Stm* then_branch);
+Stm* stm_if_then_else(Expr* condition, Stm* then_branch, Stm* else_branch);
+Stm* stm_while(Expr* condition, Stm* body);
+Stm* stm_with(Pckg* pckg_with);
+Stm* stm_use(Pckg* pckg_use);
+Stm* stm_procedure(char* proc_id, Dclr* dclr, Stm* body);
+Stm* stm_function(char* func_id, Expr* arg);
 
 #endif
